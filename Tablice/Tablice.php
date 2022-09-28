8f98eb9374da831cbb09113419be74f2e106ba5d
Kotocry <kotocrydirection@gmail.com>
2022-09-28 15:00:51 +0200

    Dodałem.

--- Tablice/Tablice.php
+++ Tablice/Tablice.php
@@ -19,5 +19,53 @@
         echo "$item: $description <br>";
         ++$a;
     }
+	
+	echo "<hr>";
+	
+	$products = array(
+                'paper' => array(
+                    'copier' => "Do kserokopiarek i uniwersalny",
+                    'inkjet' => "Do drukarek atramentowych",
+                    'laser' => "Do drukarek laserowych",
+                    'photo' => "Papier fotograficzny"),
+                'pens' => array(
+                    'ball' => "Długopisy",
+                    'hilite' => "Markery przezroczyste",
+                    'marker' => "Markery zwykłe"),
+                'misc' => array(
+                    'tape' => "Taśmy klejące",
+                    'glue' => "Kleje",
+                    'clips' => "Spinacze")
+                );
+            echo "<pre>";
+                    echo $products['paper']['copier'];
+					echo "<br>";
+					echo $products['pens']['ball'];
+					echo "<br>";
+					echo $products['misc']['tape'];
+            echo "</pre>";   
+
+		echo "<hr>";
+
+	$chessboard = array(
+        array('w','s','g','h','k','g','s','w'),
+        array('p','p','p','p','p','p','p','p'),
+        array(' ',' ',' ',' ',' ',' ',' ',' '),
+        array(' ',' ',' ',' ',' ',' ',' ',' '),
+        array(' ',' ',' ',' ',' ',' ',' ',' '),
+        array(' ',' ',' ',' ',' ',' ',' ',' '),
+        array('P','P','P','P','P','P','P','P'),
+        array('W','S','G','H','K','G','S','W'),
+    );
+    echo "<pre>";
+	$chessboard[6][3] = ' ';
+	$chessboard[4][3] = 'P';
+    foreach($chessboard as $row) 
+    {
+        foreach($row as $piece)
+            echo "$piece ";
+        echo "<br>";
+    }
+    echo "</pre>";		
 ?>
 
